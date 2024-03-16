db64b08dfe83992de7d5c76795c15866105c5674
Calyee <90994826+Yikoutian1@users.noreply.github.com>
2024-03-16 16:35:35 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,12 +9,20 @@ def list_split(items, n):
 def getdata(name):
     gitpage = requests.get("https://github.com/" + name)
     data = gitpage.text
-    datadatereg = re.compile(r'data-date="(.*?)" data-level')
-    datacountreg = re.compile(r'<span class="sr-only">(.*?) contribution')
+    
+    # 2023-11-22 更新正则 https://github.com/Zfour/python_github_calendar_api/issues/18
+    datadatereg = re.compile(r'data-date="(.*?)" id="contribution-day-component')
+    datacountreg = re.compile(r'<tool-tip .*?class="sr-only position-absolute">(.*?) contribution')
+    
     datadate = datadatereg.findall(data)
     datacount = datacountreg.findall(data)
     datacount = list(map(int, [0 if i == "No" else i for i in datacount]))
 
+    # 检查datadate和datacount是否为空
+    if not datadate or not datacount:
+        # 处理空数据情况
+        return {"total": 0, "contributions": []}
+        
     # 将datadate和datacount按照字典序排序
     sorted_data = sorted(zip(datadate, datacount))
     datadate, datacount = zip(*sorted_data)
@@ -32,8 +40,12 @@ def getdata(name):
     return returndata
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        # 2024-03-15 规范接口的传参方式 https://github.com/Zfour/python_github_calendar_api/issues/20#issuecomment-1999115747
         path = self.path
-        user = path.split('?')[1]
+        spl=path.split('?')[1:]
+        for kv in spl:
+            key,user=kv.split("=")
+            if key=="user": break
         data = getdata(user)
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
